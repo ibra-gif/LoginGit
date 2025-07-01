786081082bbf066fd444fab7e9b2113b8937fdfd
ibra-gif <144303239+ibra-gif@users.noreply.github.com>
2025-07-01 22:16:38 +0700

    Login

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.i222102536.login"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.i222102536.login"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.loopj.android:android-async-http:1.4.9")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("com.squareup.picasso:picasso:2.5.2")
+}
---
+++ app/src/main/java/com/i222102536/login/ForexModel.java
@@ -0,0 +1,13 @@
+package com.i222102536.login;
+
+public class ForexModel {
+    public String code, name;
+    public double rate;
+
+    public ForexModel(String code, String name, double rate){
+        this.code = code;
+        this.name = name;
+        this.rate = rate;
+    }
+}
+
